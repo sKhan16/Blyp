45ed1a551c0bae2c2903f9a0dec50a588b970981
Shakeel Khan <shakeelkhan@Shakeels-MacBook-Pro.local>
2020-04-24 04:43:39 -0700

    Added two swift files that (should) represent the carouself that will be placed when a user first opens up the Blyp up. Finished section 1 of the tutorial on adding a carousel.

---
+++ blyp/Views/PageView.swift
@@ -0,0 +1,29 @@
+//
+//  PageView.swift
+//  blyp
+//
+//  Created by Shakeel Khan on 4/24/20.
+//  Copyright © 2020 Team Sonar. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var viewControllers: [UIHostingController<Page>]
+
+    init(_ views: [Page]) {
+        self.viewControllers = views.map { UIHostingController(rootView: $0) }
+    }
+
+    var body: some View {
+        PageViewController(controllers: viewControllers)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(features.map { FeatureCard(landmark: $0) })
+        .aspectRatio(3/2, contentMode: .fit)
+    }
+}
---
+++ blyp/Views/PageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PageViewController.swift
+//  blyp
+//
+//  Created by Shakeel Khan on 4/24/20.
+//  Copyright © 2020 Team Sonar. All rights reserved.
+//
+
+
+import SwiftUI
+import UIKit
+
+struct PageViewController: UIViewControllerRepresentable {
+    var controllers: [UIViewController]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [controllers[0]], direction: .forward, animated: true)
+    }
+}
