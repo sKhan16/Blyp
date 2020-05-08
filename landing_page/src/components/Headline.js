bbc0ae072f0ac8f41a8c5cdcc24a26f8845c9248
Hayden Hong <hello@haydenhong.com>
2020-05-08 13:48:26 -0700

    Corrected headling lacking href

--- landing_page/src/components/Headline.js
+++ landing_page/src/components/Headline.js
@@ -7,7 +7,7 @@ export default class Headline extends React.Component {
       <h1 className="header">curate your <br></br> digital self.</h1>
         <p className="subheader">with <span className="blyp-text-logo">Blyp</span>, you can share your experiences with loved ones</p>
         
-        <a className="button button-temp" href="">coming soon to the app store</a>
+        <a className="button button-temp" href="blyp.info">coming soon to the app store</a>
         
         {/* <div className="download-button">  
             <a href="" alt="Download in the app store link"> </a>
